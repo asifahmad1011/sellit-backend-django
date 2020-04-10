f4655e483f4d733e401588f92b618202a824e852
asifahmad1011 <asif.apag@gmail.com>
2020-04-11 01:54:04 +0200

    pagination and passwrod hashing

--- users/serializers.py
+++ users/serializers.py
@@ -4,7 +4,27 @@ from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
     # snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+    # password = serializers.CharField(max_length=255,
+    # style={'input_type': 'password'})
 
     class Meta:
         model = User
         fields = ['id', 'username', 'password', 'first_name', 'last_name', 'email', 'is_staff', 'is_active']
+        # extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            if attr == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, attr, value)
+        instance.save()
+        return instance
