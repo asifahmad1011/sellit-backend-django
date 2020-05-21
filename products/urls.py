4035a92b30ed26a5d52c084333b42dbd471aa9f8
asifahmad1011 <asif.apag@gmail.com>
2020-05-21 11:18:31 +0200

    urls update

--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('products/', views.ProductsList.as_view()),
     path('products/new', views.ProductsListCreate.as_view()),
     path('productbyid/<int:pk>', views.ProductsDetail.as_view()),
+    path('updateproduct/<int:pk>', views.ProductsDetail.as_view()),
+    path('deleteproduct/<int:pk>', views.ProductsDetail.as_view()),
     path('productbysellerid/<int:seller_id>/', views.ProductsBySeller.as_view()),
 ]
